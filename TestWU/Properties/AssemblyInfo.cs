cc688ede51b361030b4553b8605930d7de7a7eab
utente_globale <utente_globale_email@sayonala.com>
2021-07-03 18:17:09 +0200

    Add project files.

---
+++ TestWU/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestWU")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestWU")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3ae3adf-be3b-4592-ae81-87189c3486ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WU/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Ninject;
+using TAP2018_19.AlarmClock.Interfaces;
+using TAP2018_19.AuctionSite.Interfaces;
+
+namespace WU {
+    class Program {
+        protected Mock<IAlarmClock> AlarmClock;
+
+
+        static void Main(string[] args) {
+
+            var kernel = new StandardKernel();
+            
+            ISiteFactory result = null;
+            try {
+                // kernel.Load(Configuration.ImplementationAssembly);
+                // result = kernel.Get<ISiteFactory>();
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
